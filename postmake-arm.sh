3d6ca3e18289a9226c16dc7d093e8ff911a3a4ac
LoSunny <LoSunny@users.noreply.github.com>
2022-10-02 20:56:04 +0800

    Added support for arm builds

---
+++ postmake-arm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd src
+g++ -O2 -Wall -o steghide  Arg.o Arguments.o AssertionFailed.o AuFile.o AuSampleValues.o DFSAPHeuristic.o BFSAPHeuristic.o BinaryIO.o BitString.o BmpFile.o BmpPaletteSampleValue.o BmpRGBSampleValue.o BmpSampleValue.o WKSConstructionHeuristic.o DMDConstructionHeuristic.o CvrStgFile.o Edge.o EdgeIterator.o EmbData.o Embedder.o EncryptionAlgorithm.o EncryptionMode.o Extractor.o Graph.o JpegFile.o JpegSampleValue.o MCryptPP.o MHashKeyGen.o MHashPP.o Matching.o MatchingAlgorithm.o ProgressOutput.o PseudoRandomSource.o RGBTriple.o RandomSource.o SampleValue.o SampleValueAdjacencyList.o Selector.o Session.o SteghideError.o Terminal.o Utils.o Vertex.o WavChunk.o WavChunkHeader.o WavChunkUnused.o WavFile.o WavFormatChunk.o WavPCMSampleValue.o error.o main.o msg.o SMDConstructionHeuristic.o  /opt/homebrew/Cellar/gettext/0.21/lib/libintl.8.dylib -ljpeg -lmcrypt -lmhash -lz 
